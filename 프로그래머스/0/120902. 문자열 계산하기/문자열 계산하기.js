4bec462a6d5bec3d9b6a0459dba2299511f23569
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-07-14 13:32:05 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,4 +1,24 @@
+const calc = {
+    "+" : (a,b) => {return a + b},
+    "-" : (a,b) => {return a - b}
+}
+
 function solution(my_string) {
-    var answer = eval(my_string);
-    return answer;
+    let order = ''
+    const my_Arr = my_string.split(' ')
+    let result = 0
+    my_Arr.forEach((elem, id) => {
+        if(id === 0) {result = +elem}
+        else {
+            if(+elem) {
+                const calcuration = calc[order]
+                result = calcuration(result, +elem)
+            }
+            else{
+                order = elem
+            }
+        }
+    
+    })
+    return result;
 }
