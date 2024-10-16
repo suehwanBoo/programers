0a775938ab36b5afd9410bfba2970a997c9be445
suehwanBoo <130543156+suehwanBoo@users.noreply.github.com>
2024-10-16 22:29:25 +0900

    [level 2] Title: 문자열 압축, Time: 2.28 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,35 +1,32 @@
-function countCompress(str, len) {
-    let count = 0;
-    let left = 0;
-    let right = left + len;
-    let result = str.length;
-    while (right < str.length){
-       if(str[left] === str[right]) {
-           count += 1;
-           left += 1;
-           right += 1;
-       } else {
-            if(count >= len) result -= Math.floor(count/len) * len - (Math.floor(count/len) + 1).toString().length;
-           left =  left - (left % len) + len
-           right = right - (right % len) + len
-           count = 0
-       }
-    }
-    if(count >= len) result -= Math.floor(count/len) * len - (Math.floor(count/len) + 1).toString().length;
-    
-    return result;
-}
-
-
 function solution(s) {
-    var answer = 0;
-    let maxDivide = Math.floor(s.length / 2)
-    let minLength = s.length;
-    for(let divide = 1; divide < maxDivide + 1; divide++) {
-        const result = countCompress(s,divide)
-        if(result < minLength) {
-            minLength = result;
-        }
+  let minLen = s.length; //현재 스트링 길이로 시작한다.
+
+  // 압축하는 문자열 길이를 1개씩부터, s의 반 까지 시도한다.
+  top: for (let n = 1; n <= s.length / 2; n++) {
+    // 이번 트라이얼의 문자열 길이 초기화한다.
+    let curLen = 0;
+    for (let i = 0; i < s.length; i += n) {
+      // 이번 트라이얼의 길이 n 만큼 테스트를하고, 일치하는 수만큼 카운터로 계산
+      // 불일치 시에 다음으로 진행
+      let counter = 1;
+      while (s.slice(i, i + n) === s.slice(i + n, i + 2 * n)) {
+        i += n;
+        counter++;
+      }
+      // 만약 하나도 일치가 없었으면, 테스트했던 문자열의 길이만큼만 더해주고,
+      // 일치한 문자열이 하나라도 존재했다면 카운터를 스트링으로 변환 후 길이를 더해줌.
+      if (counter !== 1) {
+        curLen += n + (counter + "").length;
+      } else {
+        // 1일경우 그냥 n을 더해주는데, 혹시 맨 마지막에 남은 문자열이 n이하일 경우
+        // 남는 수만 더해준다.
+        curLen += s.length < i + n ? s.slice(i, i + n).length : n;
+      }
+      // 현재까지의 최소길이보다 현재길이가 커지는 순간, 다음 트라이얼 시작.
+      if (minLen <= curLen) continue top;
     }
-    return minLength;
+    // 여기까지 살아남았으면 이번 길이가 최소길이다.
+    minLen = curLen;
+  }
+  return minLen;
 }
